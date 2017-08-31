56a606a955121b79af3c06a69e4a5a75c3b8086e
Vit Stanislav <vit@lightcurve.io>
2017-08-31 17:06:02 +0200

    Update voteDialog to use authInputs

--- src/components/voteDialog/voteDialog.test.js
+++ src/components/voteDialog/voteDialog.test.js
@@ -80,6 +80,7 @@ describe('VoteDialog', () => {
 
       expect(props.votePlaced).to.have.been.calledWith({
         account: ordinaryAccount,
+        passphrase: ordinaryAccount.passphrase,
         activePeer: props.activePeer,
         secondSecret: null,
         unvotedList: props.unvotedList,
@@ -111,12 +112,13 @@ describe('VoteDialog', () => {
         childContextTypes: { store: PropTypes.object.isRequired },
       });
       const secondPassphrase = 'test second passphrase';
-      wrapper.instance().setSecondPass('secondPassphrase', secondPassphrase);
+      wrapper.instance().handleChange('secondPassphrase', secondPassphrase);
       wrapper.find('.primary-button button').simulate('click');
 
       expect(props.votePlaced).to.have.been.calledWith({
         activePeer: props.activePeer,
         account: accountWithSecondPassphrase,
+        passphrase: accountWithSecondPassphrase.passphrase,
         votedList: props.votedList,
         unvotedList: props.unvotedList,
         secondSecret: secondPassphrase,
