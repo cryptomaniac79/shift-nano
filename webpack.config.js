a4ed32e3fdea756c1a065a30fc5b5743d7b3c621
Craig Campbell <iamcraigcampbell@gmail.com>
2017-09-12 17:17:46 -0400

    Update accent color

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 /* eslint-enable */
 
 const reactToolboxVariables = {
-  'color-primary': '#0288D1',
-  'color-primary-dark': '#0288D1',
+  'color-primary': '#1cbdc8',
+  'color-primary-dark': '#1cbdc8',
   'button-border-radius': '3px',
   'input-text-label-color': 'rgba(0,0,0,0.38)',
 };
